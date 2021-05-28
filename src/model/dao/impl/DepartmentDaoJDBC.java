a37485949f86a50d6de304ab70d1fdeb6d9d8f55
Daniel Souza <danielfsouza@gmail.com>
2021-05-27 21:38:31 -0300

    findByDepartment Implementation

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,13 @@
+package model.dao.impl;
+
+public class DepartmentDaoJDBC {
+
+	
+	/*
+	SELECT seller.*,department.Name as DepName
+	FROM seller INNER JOIN department
+	ON seller.DepartmentId = department.Id
+	WHERE DepartmentId = ?
+	ORDER BY Name
+	*/
+}
